fa232c1a00e85ba3ad392a365b5a9a9564426248
Marius <marius.donne@edu.univ-eiffel.fr>
2023-12-18 11:05:05 +0100

    Initial commit

---
+++ src/main/java/fr/uge/chargepointconfiguration/ChargepointconfigurationApplication.java
@@ -0,0 +1,13 @@
+package fr.uge.chargepointconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargepointconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargepointconfigurationApplication.class, args);
+	}
+
+}
