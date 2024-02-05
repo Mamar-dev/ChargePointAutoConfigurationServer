6b820887ad2678fc7e85fdb68d22e6c63a6db385
Adrien COSTANDI <adrien.costandi@gmail.com>
2024-02-05 13:43:15 +0200

    Les tests passent avec maven, vive la CI

--- src/main/java/fr/uge/chargepointconfiguration/ChargepointconfigurationApplication.java
+++ src/main/java/fr/uge/chargepointconfiguration/ChargepointconfigurationApplication.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import java.net.InetSocketAddress;
 
 @SpringBootApplication
-public class ChargepointconfigurationApplication implements CommandLineRunner {
+public class ChargepointconfigurationApplication /*implements CommandLineRunner*/ {
 
 	private final UserRepository userRepository;
 
@@ -24,25 +24,25 @@ public class ChargepointconfigurationApplication implements CommandLineRunner {
 		SpringApplication.run(ChargepointconfigurationApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Thread.ofPlatform().start(() -> {
-			var host = "192.168.0.5";
-			//var host = "localhost";
-			var port = 8887;
-
-			WebSocketServer server = new ConfigurationServer(new InetSocketAddress(host, port), userRepository);
-			server.run();
-		});
-
-
-
-		Users user1 = new Users("John", "john@domain.com");
-		Users user2 = new Users("Julie", "julie@domain.com");
-		userRepository.save(user1);
-		userRepository.save(user2);
-		userRepository.findAll().forEach(System.out::println);
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Thread.ofPlatform().start(() -> {
+//			var host = "192.168.0.5";
+//			//var host = "localhost";
+//			var port = 8887;
+//
+//			WebSocketServer server = new ConfigurationServer(new InetSocketAddress(host, port), userRepository);
+//			server.run();
+//		});
+//
+//
+//
+////		Users user1 = new Users("John", "john@domain.com");
+////		Users user2 = new Users("Julie", "julie@domain.com");
+////		userRepository.save(user1);
+////		userRepository.save(user2);
+////		userRepository.findAll().forEach(System.out::println);
+//	}
 
 
 
